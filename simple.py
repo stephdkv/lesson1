be181f369f160731e1a1a6ff44bc4fc0170a4ad9
Stepan <freenlol@gmail.com>
2022-11-20 13:07:13 +0300

    'lesson1'

--- simple.py
+++ simple.py
@@ -10,17 +10,7 @@ Name = input('Введите ваше имя: ')
 print(f'Привет, {Name}! Как дела?')
 
 print(float('1'))
+print(int(float('2.5')))
 print(bool(1))
 print(bool(''))
 print(bool(0))
-
-lists = [3,5,7,9,10.5]
-print(lists)
-lists.append('Python')
-print(len(lists))
-print(lists[0])
-print(lists[5])
-print(lists[1:3])
-del lists[5]
-print(lists)
-
